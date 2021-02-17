99a54d7063cde6528fc200c298358c043620182f
정 회동 <asky1193@gmail.com>
2021-02-17 16:01:24 +0900

    solution2 added

--- stackNq/stackNq/기능개발.cpp
+++ stackNq/stackNq/기능개발.cpp
@@ -34,12 +34,40 @@ vector<int> solution(vector<int> progresses, vector<int> speeds)
     return answer;
 }
 
+vector<int> solution2(vector<int> progresses, vector<int> speeds)
+{
+    vector<int> answer, time;
+    int size = progresses.size(),count =0;
+    for (int i = 0 ; i < size; i++)
+    {
+        if ((100 - progresses[i]) % speeds[i] > 0) time.push_back((100 - progresses[i]) / speeds[i] + 1);
+        else time.push_back((100 - progresses[i]) / speeds[i]);
+    }
+    int i=0, temp;
+    while (1)
+    {
+        temp = time[i];
+        if (i == size - 1) {answer.push_back(1); break;
+    }
+        for (int j=i+1; j < size; j++)
+        {
+            count++;
+            if (temp < time[j]) { i = j; answer.push_back(count); break; }
+            
+        }
+        if (answer.size()==size) break;
+    }
+
+
+    return answer;
+}
+
 int main()
 {
     
     vector<int> ans, asdf = { 93,30,55 }, zxcv = {1,30,5}; //테스트 케이스
     
-    ans = solution(asdf, zxcv);
+    ans = solution2(asdf, zxcv);
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
