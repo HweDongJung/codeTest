99cb6c93ebb2351a12897fde7a6568684ecbe46b
정 회동 <asky1193@gmail.com>
2021-02-27 12:34:26 +0900

    수박 추가 및 주석 추가

--- lv1Test/3진법 뒤집기/소스.cpp
+++ lv1Test/3진법 뒤집기/소스.cpp
@@ -11,18 +11,18 @@ int solution(int n)
     int temp=n, answer = 0;
     vector<int> ans;
 
-    for (;;)
+    for (;;) // 3진법 수로 변환하는 반복문 
     {
         if (temp < 3)
         {
             ans.push_back(temp);
             break;
         }
-        ans.push_back(temp % 3);
-        temp = temp / 3;
+        ans.push_back(temp % 3); //나눈 나머지 push
+        temp = temp / 3; //3으로 나누기 
     }
     
-    for (int i = ans.size(); i >= 1; i--)
+    for (int i = ans.size(); i >= 1; i--) //뒤집힌 3진법 수를 다시 10진법으로 변환 
     {
         temp = pow(3, ans.size()-i);
         answer += (ans[i - 1] * temp);
