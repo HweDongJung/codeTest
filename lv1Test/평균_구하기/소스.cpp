d50d8000d863a2ff7143f34b4d0e787d61ba82d3
정 회동 <asky1193@gmail.com>
2021-03-12 14:35:36 +0900

    문제 평균 구하기 추가

---
+++ lv1Test/평균_구하기/소스.cpp
@@ -0,0 +1,17 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/12944
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) 
+{
+    double answer = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        answer += arr[i];
+    }
+    
+    return answer/arr.size();
+}
