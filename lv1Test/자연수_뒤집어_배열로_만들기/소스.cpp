a86e915c238c4c2c50392bc4123cdc81780aa972
정 회동 <asky1193@gmail.com>
2021-03-12 14:14:30 +0900

    문제 자연수 뒤집어 배열로 만들기 추가

---
+++ lv1Test/자연수_뒤집어_배열로_만들기/소스.cpp
@@ -0,0 +1,28 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/12932
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n)
+{
+    vector<int> answer;
+    long long temp = n;
+    for (;;)
+    {
+        answer.push_back(temp % 10);
+        cout << temp%10 <<endl;
+        temp /= 10;
+        if (temp == 0) break;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    long long n = 12345;
+    solution(n);
+    return 0;
+}
