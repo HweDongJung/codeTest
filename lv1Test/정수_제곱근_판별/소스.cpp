bbbca0a23f1285f831cedfb9e3f193255784b2ce
정 회동 <asky1193@gmail.com>
2021-03-13 12:06:07 +0900

    문제 정수 제곱근 판별 추가

---
+++ lv1Test/정수_제곱근_판별/소스.cpp
@@ -0,0 +1,15 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/12934
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) 
+{
+    long long answer = 0;
+    answer = sqrt(n);
+    if (pow(answer, 2) != n) return -1;
+    else return pow(++answer, 2);
+
+}
