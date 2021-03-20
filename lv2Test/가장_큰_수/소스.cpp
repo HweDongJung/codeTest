7173c91b5d98fe5aadcc3cd93c12d2102744d86c
정 회동 <asky1193@gmail.com>
2021-03-20 18:54:19 +0900

    가장 큰 수 문제 풀이 추가

---
+++ lv2Test/가장_큰_수/소스.cpp
@@ -0,0 +1,34 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/42746
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+bool cmp1(int a, int b)
+{
+    return stoi(to_string(a) + to_string(b)) > stoi(to_string(b) + to_string(a));
+}
+
+string solution(vector<int> numbers)
+{
+    if (*max_element(numbers.begin(),numbers.end()) == 0 ) return "0";
+    string answer = "";
+    vector<int> a = numbers;
+    sort(a.begin(), a.end(), cmp1);
+    for (int i = 0; i < a.size(); i++) answer += to_string(a[i]);
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> a = { 30,303 };
+    sort(a.begin(), a.end(), cmp1);
+    for (int i=0; i < a.size(); i++)cout << a[i];
+    //cout << solution({303,30}) << endl;
+
+    return 0;
+}
