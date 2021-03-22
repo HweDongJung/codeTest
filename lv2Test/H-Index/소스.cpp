1010c458f1a7aa221590a127b772e4e91f4aa10e
정 회동 <asky1193@gmail.com>
2021-03-22 17:22:09 +0900

    문제 H-index, 더 맵게 풀이 추가

---
+++ lv2Test/H-Index/소스.cpp
@@ -0,0 +1,37 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/42747
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+//3 0 6 1 5 정렬 -> 6 5 3 1 0
+using namespace std;
+
+int solution(vector<int> citations) 
+{
+    int answer = 0, count =0;
+    vector<int> cit = citations;
+    sort(cit.rbegin(), cit.rend());
+    if (*max_element(cit.begin(), cit.end()) == 0) return 0;
+    for (int i = cit[0]; i >= 0; i--)
+    {
+        for (int j = 0; j < cit.size(); j++)
+        {
+            if (cit[j] >= i) count++;
+            else if (count >= i && count >= cit.size()-j) return i;
+            else
+            {
+                count = 0;
+                break;
+            }
+        }
+    }
+    answer = citations.size();
+    return answer;
+}
+
+int main()
+{
+    cout << solution({ 2,2 }) << endl;
+    
+    return 0;
+}
---
+++ lv2Test/더 맵게/소스.cpp
@@ -0,0 +1,57 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/42626
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K)
+{
+    int answer = 0, scov=0;
+    priority_queue<int, vector<int>, greater<int>> q; //우선순위 큐 선언, min heap(top이 최소값) 
+
+    for (int i = 0; i < scoville.size(); i++) q.push(scoville[i]); 
+
+    for (;;)
+    {
+        if (q.size() == 1 && q.top() < K) return -1;
+        if (q.top() >= K) break;
+        scov += q.top(); 
+        q.pop();
+        scov += (q.top() * 2); 
+        q.pop();
+        q.push(scov); 
+        scov = 0;
+        answer++; 
+    }
+
+    return answer;
+}
+
+int solution2(vector<int> scoville, int K)//내가 처음 만든 코드, 시간초과  
+{
+    int answer = 0;
+    vector<int> scovil = scoville;
+    for (;;)
+    {
+        if (*min_element(scovil.begin(), scovil.end()) >= K) break;
+        if (scovil.size() == 1 && scovil[0] < K) return -1;
+        sort(scovil.begin(), scovil.end());
+        scovil[1] = scovil[0] + (scovil[1] * 2);
+        scovil.erase(scovil.begin() + 0);
+       
+        answer++;
+    }
+
+    return answer;
+}
+
+
+int main()
+{
+    cout << solution({ 1,2,3,9,10,12 }, 7) << endl;
+
+    return 0;
+}
