2d35a458cf86199326a3d5fca4e566420f7dbee8
정 회동 <asky1193@gmail.com>
2021-03-25 12:15:03 +0900

    문제 숫자의 표현 풀이 추가

---
+++ lv2Test/숫자의_표현/소스.cpp
@@ -0,0 +1,39 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/12924
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool reach(int start, int reac);
+
+int solution(int n) 
+{
+    int answer = 1;
+
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (reach(i, n)) answer++;
+    }
+
+
+    return answer;
+}
+
+bool reach(int start, int reac)
+{
+    int temp = 0;
+    for(int i = start; temp < reac; i++)
+    {
+        temp += i;
+    }
+    if (temp == reac) return true;
+    else return false;
+}
+
+int main()
+{
+    cout << solution(15) << endl;
+
+    return 0;
+}
