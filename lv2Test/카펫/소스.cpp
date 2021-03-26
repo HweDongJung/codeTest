3d59872416b94880c941c2b92c29d427973b7c9a
정 회동 <asky1193@gmail.com>
2021-03-26 11:09:05 +0900

    문제 카펫 풀이 추가

---
+++ lv2Test/카펫/소스.cpp
@@ -0,0 +1,32 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/42842
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow)
+{
+    vector<int> answer;
+    for (int i = 1; i <= yellow; i++)
+    {
+        if (yellow % i == 0)
+        {
+            if ((i + yellow / i) * 2 + 4 == brown)
+            {
+                answer.push_back((yellow / i) + 2);
+                answer.push_back(i + 2);
+                break;
+            }
+        }
+    }
+    return answer;
+}
+
+
+int main()
+{
+    vector<int> a = solution(24, 24);
+    cout << a[0] << " " << a[1] << endl;
+    return 0;
+}
