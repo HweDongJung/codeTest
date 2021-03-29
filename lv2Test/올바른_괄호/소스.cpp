51ffcbdf6cd4e9f1a1f461cf2aa8336ae87623ac
정 회동 <asky1193@gmail.com>
2021-03-29 09:34:27 +0900

    올바른 괄호 문제 풀이 추가

---
+++ lv2Test/올바른_괄호/소스.cpp
@@ -0,0 +1,27 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/12909
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int open = 0, close = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(') open++;
+        else close++;
+        if (open < close) return false;
+    }
+    if (open == close) return true;
+    else return false;
+
+}
+
+int main()
+{
+    cout << solution("()()") << endl;
+
+    return 0;
+}
